636f0cca5234693152d25e8a6f7f2e812b39fa16
Ivaylo Iliev <ranapat@yahoo.com>
2017-08-27 10:29:56 +0200

    hello world json

--- src/nasa/src/AppBundle/Controller/DefaultController.php
+++ src/nasa/src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,15 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
+     * Hello world
+     *
+     * Gets some json
+     *
      * @Route("/", name="homepage")
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->json([ 'hello' => 'world!' ]);
     }
 }
